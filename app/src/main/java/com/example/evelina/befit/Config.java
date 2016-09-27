c1c5b1614fa1923e809a0ac3e25ee4e08a9b1054
Ivet Metodieva <ivet_metodieva21@abv.bg>
2016-09-27 23:09:33 +0300

    Youtubeview working,app registered in Google API's

---
+++ app/src/main/java/com/example/evelina/befit/Config.java
@@ -0,0 +1,12 @@
+package com.example.evelina.befit;
+
+/**
+ * Created by User on 27-Sep-16.
+ */
+
+public class Config {
+    public static final String DEVELOPER_KEY = "AIzaSyAMssAPmtigPknRGhye_j71MPMaFjWDFZE";
+    //this here is a test
+    //video id
+    public static final String YOUTUBE_VIDEO_CODE = "_oEA18Y8gM0";
+}
